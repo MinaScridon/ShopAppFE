3fa610b16a0df88949a75fbe1b0b3184c2e9a9be
MinaScridon <110894715+MinaScridon@users.noreply.github.com>
2022-12-07 16:40:56 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { DetailsComponent } from './components/details/details.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { ProductService } from './service/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    NavBarComponent,
+    OrdersComponent,
+    AboutUsComponent,
+    AdminComponent,
+    DetailsComponent,
+    AddProductComponent,
+    EditProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+
+})
+export class AppModule { }
